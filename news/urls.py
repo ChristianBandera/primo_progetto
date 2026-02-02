69a1fcf989b585a6df8a4325c43a098a7128ff85
ChristianBandera <christianbandera07@gmail.com>
2026-02-02 12:15:29 +0100

    finito passo 20 e 21

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from news.views import homepage, articoloDetailView, index, listaArticoli, queryBase
+from news.views import homepage, articoloDetailView, index, listaArticoli, queryBase, giornalistaDetailView
 
 app_name = "news"
 
@@ -10,5 +10,6 @@ urlpatterns = [
     #int è il numero che metto e rappresenta pk che è la primary key del database
     path("lista_articoli/<int:pk>", listaArticoli, name="lista_articoli_giornalista"),
     path("lista_articoli", listaArticoli, name="lista_articoli"),
-    path("query_base", queryBase, name="query_base")
+    path("query_base", queryBase, name="query_base"),
+    path("giornalista_detail/<int:pk>", giornalistaDetailView, name="giornalista_detail")
 ]
