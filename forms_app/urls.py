07dba11066d5baaff07eb42c2d0d057759ad1941
ChristianBandera <christianbandera07@gmail.com>
2026-03-02 13:59:15 +0100

    errori nei tasti modifica e cancella contatto

--- forms_app/urls.py
+++ forms_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from forms_app.views import contatti, listaContatti, index
+from forms_app.views import contatti, listaContatti, index, modifica_contatto, elimina_contatto
 
 app_name = "forms_app"
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('index/', index, name='index'),
     path('contattaci/', contatti, name='contatti'),
     path('lista_contatti/', listaContatti, name='lista_contatti'),
+    path('elimina-contatto/<int:pk>', elimina_contatto, name='elimina-contatto'),
+    path('modifica-contatto/<int:pk>', modifica_contatto, name='modifica-contatto'),
 ]
